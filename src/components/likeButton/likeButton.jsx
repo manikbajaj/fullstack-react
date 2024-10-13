45f23e33f2feb04c02083bae3ddb5d80c300a58c
Manik <manik@pixelconception.com>
2024-10-13 20:26:56 +0530

    Final code for FSDB750

--- src/components/likeButton/likeButton.jsx
+++ src/components/likeButton/likeButton.jsx
@@ -1,3 +1,11 @@
 export default function LikeButton() {
-  return <button>0 Likes</button>;
+  let likes = 0;
+  function countLikes(e) {
+    likes++;
+    console.log(likes);
+    console.log(e);
+  }
+
+  // Event if lkes variable is added to text React does not update the text
+  return <button onClick={countLikes}>{likes} Likes</button>;
 }
