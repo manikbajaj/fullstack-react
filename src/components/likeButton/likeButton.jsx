4ce7f0b49a59cf076353477975e4062e208fa320
Manik <manik@pixelconception.com>
2024-10-13 20:48:00 +0530

    updating the planning branch

--- src/components/likeButton/likeButton.jsx
+++ src/components/likeButton/likeButton.jsx
@@ -1,9 +1,13 @@
+// Hooks are functions that let you “hook into” React state and lifecycle features from function components. They were introduced in React 16.8.
+// Purpose: Hooks provide a way to use stateful logic and side effects in functional components without writing a class.
+import { useState } from "react";
+
 export default function LikeButton() {
-  let likes = 0;
+  let [likes, setLikes] = useState(0);
+
   function countLikes(e) {
-    likes++;
-    console.log(likes);
-    console.log(e);
+    // When you pass likes++ to setLikes, you are effectively setting the state to the current value of likes before it is incremented. This means the state does not actually change on the first click, which is likely not what you intended.
+    setLikes(likes + 1);
   }
 
   // Event if lkes variable is added to text React does not update the text
