b6ef9906b271db9fda363f6a626dd5d69ff0223a
Manik <manik@pixelconception.com>
2024-10-13 12:34:14 +0530

    Final code for FSDB745

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -1,11 +1,13 @@
-// import React from "react";
-import "./userProfile.css";
+import styles from "./userProfile.module.css";
 
 export default function UserProfile() {
   return (
     <>
       <section>
-        <ul class="user-details">
+        {/* {<ul className={styles["user-details"]}>} */}
+        {/* <ul className={styles.userDetails}> */}
+        {/* Multiple Styles can be applied and styles are scoped to each module */}
+        <ul className={`${styles.userDetails} ${styles.border}`}>
           <li>First Name: John</li>
           <li>Last Name: Doe</li>
           <li>Skills: React, JavaScript</li>
