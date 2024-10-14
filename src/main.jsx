7777d8050207923b2d9c261048be00dad0c53f16
Manik <manik@pixelconception.com>
2024-10-14 17:06:26 +0530

    Final code for FSDB773

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import {
+  Link,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
