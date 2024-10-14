8825c53523a53e9023865b211bf30026ad135dbd
Manik <manik@pixelconception.com>
2024-10-14 22:33:00 +0530

    Final code for FSDB782

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,20 @@
+import { Button } from "@/components/ui/button.jsx";
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <>
-      <h1 class="text-xl">This is the Login Page</h1>
+      <h1 className="text-xl">This is the Login Page</h1>
       <ul>
         <li>
           <Link to="tasks">Tasks</Link>
         </li>
-        <Link to="signup">Signup</Link>
+        <li>
+          {/* asChild prop indicates that the Button component should treat its child elements as the button element itself, often used for semantic or accessibility reasons. It enables wrapping external components, like a Link, with Button styling without duplicating button functionality. */}
+          <Button asChild>
+            <Link to="signup">Signup</Link>
+          </Button>
+        </li>
       </ul>
     </>
   );
