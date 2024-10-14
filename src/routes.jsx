3bed1f1fda3de6235e9979ecaecc80a893454030
Manik <manik@pixelconception.com>
2024-10-14 17:57:28 +0530

    Final code for FSDB776

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,5 @@
 import Login from "./pages/login/login.jsx";
+import PageNotFound from "./pages/404/404.jsx";
 import Signup from "./pages/signup/signup.jsx";
 import Tasks from "./pages/tasks/tasks.jsx";
 import { createBrowserRouter } from "react-router-dom";
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: "signup",
     element: <Signup />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
   },
 ]);
