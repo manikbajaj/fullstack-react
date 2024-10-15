9488ef6ba3f183a65a0cc04a8b3565fb8b6233bb
Manik <manik@pixelconception.com>
2024-10-15 17:20:59 +0530

    final code for FSDB787

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,21 +1,40 @@
-import { Button } from "@/components/ui/button.jsx";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <>
-      <h1 className="text-xl">This is the Login Page</h1>
-      <ul>
-        <li>
-          <Link to="tasks">Tasks</Link>
-        </li>
-        <li>
-          {/* asChild prop indicates that the Button component should treat its child elements as the button element itself, often used for semantic or accessibility reasons. It enables wrapping external components, like a Link, with Button styling without duplicating button functionality. */}
-          <Button asChild>
-            <Link to="signup">Signup</Link>
-          </Button>
-        </li>
-      </ul>
-    </>
+    <section className="flex flex-row max-w-screen-xl min-h-screen w-full justify-center items-center">
+      <div className="w-4/12">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Login and Create Tasks</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Input className="mb-4" type="email" placeholder="Email" />
+            <Input className="mb-4" type="password" placeholder="Password" />
+          </CardContent>
+          <CardFooter className="flex flex-row justify-between">
+            <p className="basis-1/2">
+              Don't have an account?{" "}
+              <Link className="text-blue-500 hover:text-blue-700" to="/signup">
+                Signup Here
+              </Link>
+            </p>
+            <Button>Login</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
   );
 }
