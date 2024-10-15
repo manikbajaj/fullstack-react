40a11f93dfdee9635d8314e9895c46d571348fb9
Manik <manik@pixelconception.com>
2024-10-15 17:32:43 +0530

    Final code for FSDB788

--- src/pages/tasks/tasks.jsx
+++ src/pages/tasks/tasks.jsx
@@ -1,3 +1,12 @@
 export default function Tasks() {
-  return <>Tasks Page</>;
+  return (
+    <section className="flex flex-row w-full p-4 gap-8 bg-yellow-400 h-20">
+      <section className="flex basis-2/3 bg-pink-400">
+        <p>Tasks Section</p>
+      </section>
+      <section className="flex basis-1/3 bg-pink-400">
+        Create Task Section
+      </section>
+    </section>
+  );
 }
