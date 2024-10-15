99990c79b0db77763a35c0743068bdfda24b4f8c
Manik <manik@pixelconception.com>
2024-10-15 17:12:07 +0530

    Final code for FSDB786

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -7,10 +7,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
     <section className="flex flex-row max-w-screen-xl min-h-screen w-full justify-center items-center">
-      <div className="h-20 w-4/12">
+      <div className="w-4/12">
         <Card>
           <CardHeader>
             <CardTitle>Signup</CardTitle>
@@ -19,10 +23,19 @@ export default function Signup() {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <Input className="mb-4" type="text" placeholder="First Name" />
+            <Input className="mb-4" type="text" placeholder="Last Name" />
+            <Input className="mb-4" type="email" placeholder="Email" />
+            <Input className="mb-4" type="password" placeholder="Password" />
           </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
+          <CardFooter className="flex flex-row justify-between">
+            <p className="basis-1/2">
+              Already have an account?{" "}
+              <Link className="text-blue-500 hover:text-blue-700" to="/">
+                Login Here
+              </Link>
+            </p>
+            <Button>Signup</Button>
           </CardFooter>
         </Card>
       </div>
