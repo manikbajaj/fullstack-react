f051dee218b3ffc689978e79841f96c1c68f669a
Manik <manik@pixelconception.com>
2024-10-15 16:16:05 +0530

    Final code for FSDB785

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,3 +1,31 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Signup() {
-  return <>Signup Page</>;
+  return (
+    <section className="flex flex-row max-w-screen-xl min-h-screen w-full justify-center items-center">
+      <div className="h-20 w-4/12">
+        <Card>
+          <CardHeader>
+            <CardTitle>Signup</CardTitle>
+            <CardDescription>
+              Create a new account to start creating tasks
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+  );
 }
