3d56cc0d49f8896068cc6431a900e37a04370c32
Manik <manik@pixelconception.com>
2024-10-21 16:47:56 +0530

    Final code for FSDB813

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -6,12 +6,32 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
+import { SignupSchema } from "@/schema/signup.schema.js";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function Signup() {
+  // 1. Define your form.
+  const form = useForm({
+    resolver: zodResolver(SignupSchema),
+  });
+
+  /** Function to handle what will happen when the form is submitted */
+  function onSubmit(values) {
+    console.log(values);
+  }
+
   return (
     <section className="flex flex-row max-w-screen-xl min-h-screen w-full justify-center items-center">
       <div className="w-4/12">
@@ -22,21 +42,76 @@ export default function Signup() {
               Create a new account to start creating tasks
             </CardDescription>
           </CardHeader>
-          <CardContent>
-            <Input className="mb-4" type="text" placeholder="First Name" />
-            <Input className="mb-4" type="text" placeholder="Last Name" />
-            <Input className="mb-4" type="email" placeholder="Email" />
-            <Input className="mb-4" type="password" placeholder="Password" />
-          </CardContent>
-          <CardFooter className="flex flex-row justify-between">
-            <p className="basis-1/2">
-              Already have an account?{" "}
-              <Link className="text-blue-500 hover:text-blue-700" to="/">
-                Login Here
-              </Link>
-            </p>
-            <Button>Signup</Button>
-          </CardFooter>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <CardContent>
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="First Name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="Last Name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter className="flex flex-row justify-between">
+                <p className="basis-1/2">
+                  Already have an account?{" "}
+                  <Link className="text-blue-500 hover:text-blue-700" to="/">
+                    Login Here
+                  </Link>
+                </p>
+                <Button type="submit">Signup</Button>
+              </CardFooter>
+            </form>
+          </Form>
         </Card>
       </div>
     </section>
--- src/schema/signup.schema.js
+++ src/schema/signup.schema.js
@@ -5,7 +5,7 @@ const passwordValidation = new RegExp(
   /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
 );
 
-const SignupSchema = z.object({
+export const SignupSchema = z.object({
   firstName: z
     .string()
     .min(3, {
@@ -15,21 +15,17 @@ const SignupSchema = z.object({
     .max(100, {
       message: "First name cannot be more than 100 characters",
     }),
-  lastName: z.string().optional().max(100, {
-    message: "Last name cannot be more than 100 characters",
-  }),
-  email: z.email().min(5, {
-    // using min makes it a required field
-    message: "Email must be at least 5 characters.",
-  }),
-  password: z
-    .password()
-    .min(8, {
-      // using min makes it a required field
-      message: "Password must be at least 8 characters.",
+  lastName: z
+    .string()
+    .max(100, {
+      message: "Last name cannot be more than 100 characters",
     })
-    .regex(passwordValidation, {
-      message:
-        "Password must include at least one number, one uppercase letter, one lowercase letter, and one special character.",
-    }),
+    .optional(), // optional needs to come after max
+  email: z.string().email({
+    message: "Invalid email format.",
+  }),
+  password: z.string().regex(passwordValidation, {
+    message:
+      "Password must be atleast 8 characters and must include at least one number, one uppercase letter, one lowercase letter, and one special character.",
+  }),
 });
