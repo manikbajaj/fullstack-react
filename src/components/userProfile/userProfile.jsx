27389f23c4c3e81f0a5e4382ccb3d1df8f4a0af7
Manik <manik@pixelconception.com>
2024-10-21 12:17:21 +0530

    Final coed for FSDB795

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -1,3 +1,21 @@
-export function UserProfile() {
-  return <div>User Profile</div>;
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+import PropTypes from "prop-types";
+
+export function UserProfile(props) {
+  const { firstName = "John" } = props;
+  return (
+    <div className="flex flex-col w-full py-8 items-center">
+      <Avatar className="mb-4">
+        <AvatarFallback className="text-2xl font-semibold">
+          {firstName.slice(0, 1)}
+        </AvatarFallback>
+      </Avatar>
+      <h4>Hello, {firstName}</h4>
+    </div>
+  );
 }
+
+UserProfile.propTypes = {
+  firstName: PropTypes.string,
+};
