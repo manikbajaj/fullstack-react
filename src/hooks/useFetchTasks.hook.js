306dd0c5b3517380b39c0b36862a3b571aa44678
Manik <manik@pixelconception.com>
2024-10-22 16:43:17 +0530

    Final code for FSDB839

---
+++ src/hooks/useFetchTasks.hook.js
@@ -0,0 +1,37 @@
+import Cookies from "js-cookie";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchTasks = async ({ queryKey }) => {
+  const [_key, { order = "asc", limit = 10, page = 1 }] = queryKey;
+  const token = Cookies.get("token");
+
+  const url = new URL(`${import.meta.env.VITE_API_URL}tasks`);
+  url.searchParams.append("order", order);
+  url.searchParams.append("limit", limit);
+  url.searchParams.append("page", page);
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
+export function useFetchTasks(params = {}) {
+  return useQuery({
+    queryKey: ["fetchTasks", params],
+    queryFn: fetchTasks,
+    onSuccess: (response) => {
+      console.log("Tasks fetched successfully:", response);
+    },
+    onError: (error) => {
+      console.error("Error fetching tasks:", error);
+    },
+  });
+}
