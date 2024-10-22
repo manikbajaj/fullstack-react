9d22f6cb09aa7e798b86c31c7807984620d27a83
Manik <manik@pixelconception.com>
2024-10-22 10:45:24 +0530

    Adding env file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-console.log(path.resolve(__dirname, "./"));
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
