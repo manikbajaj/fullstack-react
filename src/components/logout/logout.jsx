26edf20b0d8c341a60e39ae1ffe812ed46174168
Manik <manik@pixelconception.com>
2024-10-22 17:17:11 +0530

    Final code for FSDB841

--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import Cookies from "js-cookie";
 import { LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export function Logout() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    Cookies.remove("token");
+    navigate("/");
+  }
+
   return (
     <div className="flex justify-end">
-      <Button variant="outline" size="icon">
+      <Button variant="outline" size="icon" onClick={handleClick}>
         <LogOut className="h-4 w-4" />
       </Button>
     </div>
